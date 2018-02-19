e0e562b31ae3b0aadfeeb94fe64e89eee283b327
Olushi Luqman O <olushilukmon03@gmail.com>
2018-02-19 21:41:26 +0100

    middleware hook - hash password pre save

--- db/models/company.js
+++ db/models/company.js
@@ -1,4 +1,5 @@
 const mongoose = require('mongoose')
+const bcrypt = require('bcrypt')
 
 const companySchema = mongoose.Schema({
   name: { type: String, required: true },
@@ -6,6 +7,21 @@ const companySchema = mongoose.Schema({
   password: { type: String, required: true },
   isVerified: {type: Boolean, default: false}
   // TODO: add timestamps to remove accounts not verified for a period of time
+})
+
+companySchema.pre('save', function (next) {
+  const company = this
+
+  bcrypt.genSalt(10, (err, salt) => {
+    if (err) return next(err)
+
+    bcrypt.hash(company.password, salt, (err, encryptedPassword) => {
+      if (err) return next(err)
+
+      company.password = encryptedPassword
+      next()
+    })
+  })
 })
 
 mongoose.model('company', companySchema)
