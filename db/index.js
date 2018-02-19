5f7cd508eaf5fcf577493a30dfc6b482763522b9
Olushi Luqman O <olushilukmon03@gmail.com>
2018-02-19 21:39:17 +0100

    use native promise for mongoose

--- db/index.js
+++ db/index.js
@@ -2,6 +2,6 @@ const mongoose = require('mongoose')
 const config = require('../config/keys')
 
 mongoose.connect(config.database)
-
+mongoose.Promise = global.Promise
 // models
 require('./models')
