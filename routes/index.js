c3a7ce7ed7d9d595ed24df7473f8c3eec28d33a6
Olushi Luqman O <olushilukmon03@gmail.com>
2018-02-20 23:57:11 +0100

    register service

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,4 @@
 module.exports = (app) => {
-    // test route
-  app.get('/', (req, res) => {
-    res.send('Hello, world')
-  })
-
-  // register auth route
+  require('../services/passport')
   require('./auth')(app)
 }
