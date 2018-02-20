d896754b47f9e1ac4ef4e559da3e28a30bb3096a
Olushi Luqman O <olushilukmon03@gmail.com>
2018-02-20 23:52:03 +0100

    authenticate route

--- routes/auth.js
+++ routes/auth.js
@@ -1,7 +1,10 @@
 const {signupController} = require('./controllers/authController')
+const passport = require('passport')
+
 module.exports = (app) => {
-  app.post('/signin', (req, res) => {
-      // signin logic
+  app.post('/signin', passport.authenticate('local', {session: false}), function (req, res) {
+    res.send({user: req.user})
   })
+
   app.post('/signup', signupController)
 }
