aa8de311024c8ec6ac3ab625f1a0baed1ce45238
Olushi Luqman O <olushilukmon03@gmail.com>
2018-02-20 15:10:42 +0100

    use controller in auth route

--- routes/auth.js
+++ routes/auth.js
@@ -1,8 +1,7 @@
+const {signupController} = require('./controllers/authController')
 module.exports = (app) => {
   app.post('/signin', (req, res) => {
       // signin logic
   })
-  app.post('/signup', (req, res) => {
-      // singup logic
-  })
+  app.post('/signup', signupController)
 }
