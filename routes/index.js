6188297e418d0bd20bbf8a32dfb921224239c667
Olushi Luqman O <olushilukmon03@gmail.com>
2018-02-21 11:51:29 +0100

    protect route with jwt

--- routes/index.js
+++ routes/index.js
@@ -1,4 +1,13 @@
+const passport = require('passport')
+
+const requireToken = passport.authenticate('jwt', {session: false})
+
 module.exports = (app) => {
   require('../services/passport')
   require('./auth')(app)
+
+  // protected resource
+  app.get('/profile', requireToken, (req, res) => {
+    res.send('hello world')
+  })
 }
