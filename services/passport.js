41869b82828c3cd219e4b3c89d01c68e8cfe7eef
Olushi Luqman O <olushilukmon03@gmail.com>
2018-02-21 11:43:40 +0100

    configure passport-jwt strategy

--- services/passport.js
+++ services/passport.js
@@ -1,6 +1,10 @@
 const passport = require('passport')
 const Company = require('mongoose').model('company')
 const LocalStrategy = require('passport-local').Strategy
+const passportJwt = require('passport-jwt')
+const JwtStrategy = passportJwt.Strategy
+const ExtractJwt = passportJwt.ExtractJwt
+const {jwtSecret} = require('../config/keys')
 
 const localOptions = {usernameField: 'email'}
 passport.use(new LocalStrategy(localOptions, (email, password, done) => {
@@ -14,5 +18,19 @@ passport.use(new LocalStrategy(localOptions, (email, password, done) => {
 
       return done(null, company)
     })
+  })
+}))
+
+const jwtOptions = {
+  secretOrKey: jwtSecret,
+  jwtFromRequest: ExtractJwt.fromAuthHeaderAsBearerToken()
+}
+
+passport.use(new JwtStrategy(jwtOptions, (payload, done) => {
+  Company.findById(payload.sub, (err, company) => {
+    if (err) { return done(err, false) }
+    if (!company) { return done(null, false, {message: 'Authentication failed'}) }
+
+    return done(null, company)
   })
 }))
