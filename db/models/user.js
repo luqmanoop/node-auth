c0c661ef5ea5170722fc06259ceb5df50489895b
Olushi Luqman O <olushilukmon03@gmail.com>
2018-02-22 21:40:18 +0100

    updated schema and model name

---
+++ db/models/user.js
@@ -0,0 +1,32 @@
+const mongoose = require('mongoose')
+const bcrypt = require('bcrypt')
+
+const userSchema = mongoose.Schema({
+  email: { type: String, unique: true, lowercase: true, required: true },
+  password: { type: String, required: true }
+})
+
+userSchema.pre('save', function (next) {
+  const user = this
+
+  bcrypt.genSalt(10, (err, salt) => {
+    if (err) return next(err)
+
+    bcrypt.hash(user.password, salt, (err, encryptedPassword) => {
+      if (err) return next(err)
+
+      user.password = encryptedPassword
+      next()
+    })
+  })
+})
+
+userSchema.methods.verifyPassword = function (userPassword, callback) {
+  bcrypt.compare(userPassword, this.password, (err, isMatch) => {
+    if (err) return callback(err)
+
+    callback(null, isMatch)
+  })
+}
+
+mongoose.model('user', userSchema)
