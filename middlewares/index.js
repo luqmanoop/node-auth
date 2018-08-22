633e59b07581ebe3e523bb13185de0c423379688
Olushi Luqman O <olushilukmon03@gmail.com>
2018-08-22 17:23:45 +0100

    add morgan middleware

--- middlewares/index.js
+++ middlewares/index.js
@@ -1,10 +1,12 @@
-const cors = require('cors')
-const bodyParser = require('body-parser')
-const passport = require('passport')
+const cors = require('cors');
+const bodyParser = require('body-parser');
+const passport = require('passport');
+const morgan = require('morgan');
 
-module.exports = (app) => {
-  app.use(cors())
-  app.use(bodyParser.json())
-  app.use(bodyParser.urlencoded({extended: true}))
-  app.use(passport.initialize())
-}
+module.exports = app => {
+  app.use(cors());
+  app.use(morgan('dev'));
+  app.use(bodyParser.json());
+  app.use(bodyParser.urlencoded({ extended: true }));
+  app.use(passport.initialize());
+};
