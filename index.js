6edebd05461369ade9af5d6a3c3ec304766ad6a2
Olushi Luqman O <olushilukmon03@gmail.com>
2018-08-22 17:22:12 +0100

    change dev port

--- index.js
+++ index.js
@@ -1,13 +1,15 @@
-const express = require('express')
-const appMiddlewares = require('./middlewares')
-const router = require('./routes')
-const app = express()
+const express = require('express');
+const appMiddlewares = require('./middlewares');
+const router = require('./routes');
+const app = express();
 
-require('./db')
-appMiddlewares(app)
-router(app)
+require('./db');
+appMiddlewares(app);
+router(app);
 
-const PORT = process.env.PORT || 1234
+const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
-  console.log(`Server listening on port ${PORT}. Visit http://localhost:${PORT}`)
-})
+  console.log(
+    `Server listening on port ${PORT}. Visit http://localhost:${PORT}`
+  );
+});
